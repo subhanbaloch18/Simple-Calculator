491ab98e89a73dbab830c19e854d43a6f070e36a
Subhan Baloch <160255991+subhanbaloch18@users.noreply.github.com>
2024-03-20 08:45:05 -0700

    Tweak the text and added console at the end

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "select one of the operators to perform operation",
         type: "list",
@@ -27,3 +27,4 @@ else {
     console.log("Please select valid operator");
 }
 ;
+console.log('END');
